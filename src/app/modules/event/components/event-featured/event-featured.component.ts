2b333dad4dbf50a1b3cd2b749b5b08e3e8684c38
Raven <55733870+imlodo@users.noreply.github.com>
2024-02-25 16:31:21 +0100

    Implementato dark mode e sistemato homepage

--- src/app/modules/event/components/event-featured/event-featured.component.ts
+++ src/app/modules/event/components/event-featured/event-featured.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
 
@@ -7,8 +7,19 @@ import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
   templateUrl: './event-featured.component.html',
   styleUrls: ['./event-featured.component.scss']
 })
-export class EventFeaturedComponent {
+export class EventFeaturedComponent implements AfterViewChecked{
   constructor(private router: Router) {
+  }
+
+  protected darkMode = false;
+
+  ngAfterViewChecked(): void {
+    let darkModeChoice = localStorage.getItem("darkModeChoice");
+    if (darkModeChoice === "0") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = true;
+    }
   }
 
   goToFeaturedEvent() {
--- src/app/modules/topic/components/featured-topic/featured-topic.component.ts
+++ src/app/modules/topic/components/featured-topic/featured-topic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
 
@@ -7,8 +7,19 @@ import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
   templateUrl: './featured-topic.component.html',
   styleUrls: ['./featured-topic.component.scss']
 })
-export class FeaturedTopicComponent {
+export class FeaturedTopicComponent implements AfterViewChecked {
   constructor(private router: Router) {
+  }
+
+  protected darkMode = false;
+
+  ngAfterViewChecked(): void {
+    let darkModeChoice = localStorage.getItem("darkModeChoice");
+    if (darkModeChoice === "0") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = true;
+    }
   }
 
   goToTopicList() {
