d091ba1bcaacda7996ec3b0071954d3db9f73162
Raven <55733870+imlodo@users.noreply.github.com>
2024-02-26 00:48:32 +0100

    update messages

--- src/app/modules/message/components/message-list/message-list.component.ts
+++ src/app/modules/message/components/message-list/message-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'unievent-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.scss']
 })
-export class MessageListComponent {
+export class MessageListComponent implements AfterViewChecked {
+  protected darkMode = false;
+  constructor(private router: Router) {
+  }
 
+  ngAfterViewChecked(): void {
+    let darkModeChoice = localStorage.getItem("darkModeChoice");
+    if (darkModeChoice === "0") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = true;
+    }
+  }
 }
