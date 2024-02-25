7f2542e7d1a4958c1f247c1d80236e885eb02121
Raven <55733870+imlodo@users.noreply.github.com>
2024-02-25 01:04:44 +0100

    Update homepage dark mode
    Update homepage dark mode

--- src/app/modules/artist/components/artist-featured/artist-featured.component.ts
+++ src/app/modules/artist/components/artist-featured/artist-featured.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
 
@@ -7,9 +7,20 @@ import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
   templateUrl: './artist-featured.component.html',
   styleUrls: ['./artist-featured.component.scss']
 })
-export class ArtistFeaturedComponent {
+export class ArtistFeaturedComponent implements AfterViewChecked {
+  currentYear: number = new Date().getFullYear();
+  protected darkMode = false;
 
   constructor(private router: Router) {
+  }
+
+  ngAfterViewChecked(): void {
+    let darkModeChoice = localStorage.getItem("darkModeChoice");
+    if (darkModeChoice === "0") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = true;
+    }
   }
 
   goToAristList() {
--- src/app/modules/event/components/event-near/event-near.component.ts
+++ src/app/modules/event/components/event-near/event-near.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
 
@@ -7,11 +7,22 @@ import { ROUTE_LIST } from 'src/app/core/utility/global-constant';
   templateUrl: './event-near.component.html',
   styleUrls: ['./event-near.component.scss']
 })
-export class EventNearComponent {
-  
+export class EventNearComponent implements AfterViewChecked {
+  currentYear: number = new Date().getFullYear();
+  protected darkMode = false;
+
   constructor(private router: Router) {
   }
 
+  ngAfterViewChecked(): void {
+    let darkModeChoice = localStorage.getItem("darkModeChoice");
+    if (darkModeChoice === "0") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = true;
+    }
+  }
+  
   goToNearEvent() {
     this.router.navigate([ROUTE_LIST.event.near.list]);
   }
