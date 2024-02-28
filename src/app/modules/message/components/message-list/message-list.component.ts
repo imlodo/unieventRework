75a14189e8541803b03af7bd3fc0782bd3f1ebf6
Raven <55733870+imlodo@users.noreply.github.com>
2024-02-29 00:38:33 +0100

    ottimizzazione codice
    ottimizzazione codice

--- src/app/modules/message/components/message-list/message-list.component.ts
+++ src/app/modules/message/components/message-list/message-list.component.ts
@@ -13,11 +13,11 @@ import { ChatDate } from '../../models/chat-date';
   styleUrls: ['./message-list.component.scss']
 })
 export class MessageListComponent implements AfterViewChecked {
-  @ViewChild('messageInput') protected messageInput: ElementRef;
-  protected darkMode = false;
-  protected messageValue: string = '';
-  protected filteredListByActiveChat: Chat;
-  protected currentUser = {
+  @ViewChild('messageInput') messageInput: ElementRef;
+  darkMode = false;
+  messageValue = '';
+  activeChatUser: User = null;
+  currentUser: User = {
     t_username: null,
     t_password: null,
     t_name: "Antonio",
@@ -26,8 +26,7 @@ export class MessageListComponent implements AfterViewChecked {
     t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102",
     t_type: USER_TYPE.CUSTOMER
   };
-  protected activeChatUser: User = null;
-  protected chatList: Array<Chat> = [
+  chatList: Chat[] = [
     {
       userChat: {
         t_username: null,
@@ -40,46 +39,14 @@ export class MessageListComponent implements AfterViewChecked {
       },
       messages: [
         {
-          user_to: {
-            t_username: null,
-            t_password: null,
-            t_name: "Antonio",
-            t_surname: "Baldi",
-            t_alias_generated: "baldilodo",
-            t_profile_photo: "https://staff.polito.it/mario.baldi/images/Mario%20202004.jpg",
-            t_type: USER_TYPE.CUSTOMER
-          },
-          user_at: {
-            t_username: null,
-            t_password: null,
-            t_name: "Antonio",
-            t_surname: "Lodato",
-            t_alias_generated: "lodo",
-            t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102",
-            t_type: USER_TYPE.CUSTOMER
-          },
+          user_to: { t_username: null, t_password: null, t_name: "Antonio", t_surname: "Baldi", t_alias_generated: "baldilodo", t_profile_photo: "https://staff.polito.it/mario.baldi/images/Mario%20202004.jpg", t_type: USER_TYPE.CUSTOMER },
+          user_at: { t_username: null, t_password: null, t_name: "Antonio", t_surname: "Lodato", t_alias_generated: "lodo", t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102", t_type: USER_TYPE.CUSTOMER },
           message: "Ciao, come stai?",
           dateTime: moment("15/02/2024 12:40PM", "D/M/YYYY HH:mm:ss")
         },
         {
-          user_to: {
-            t_username: null,
-            t_password: null,
-            t_name: "Antonio",
-            t_surname: "Lodato",
-            t_alias_generated: "lodo",
-            t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102",
-            t_type: USER_TYPE.CUSTOMER
-          },
-          user_at: {
-            t_username: null,
-            t_password: null,
-            t_name: "Antonio",
-            t_surname: "Baldi",
-            t_alias_generated: "baldilodo",
-            t_profile_photo: "https://staff.polito.it/mario.baldi/images/Mario%20202004.jpg",
-            t_type: USER_TYPE.CUSTOMER
-          },
+          user_to: { t_username: null, t_password: null, t_name: "Antonio", t_surname: "Lodato", t_alias_generated: "lodo", t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102", t_type: USER_TYPE.CUSTOMER },
+          user_at: { t_username: null, t_password: null, t_name: "Antonio", t_surname: "Baldi", t_alias_generated: "baldilodo", t_profile_photo: "https://staff.polito.it/mario.baldi/images/Mario%20202004.jpg", t_type: USER_TYPE.CUSTOMER },
           message: "Bene e tu?",
           dateTime: moment("15/02/2024 12:42PM", "D/M/YYYY HH:mm:ss")
         }
@@ -97,132 +64,101 @@ export class MessageListComponent implements AfterViewChecked {
       },
       messages: [
         {
-          user_to: {
-            t_username: null,
-            t_password: null,
-            t_name: "Maria",
-            t_surname: "Politano",
-            t_alias_generated: "mariapolitano1",
-            t_profile_photo: "https://cdn.21buttons.com/posts/640x799/a4f98433206c47f3ac3b47039996f26f_1080x1349.jpg",
-            t_type: USER_TYPE.CUSTOMER
-          },
-          user_at: {
-            t_username: null,
-            t_password: null,
-            t_name: "Antonio",
-            t_surname: "Lodato",
-            t_alias_generated: "lodo",
-            t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102",
-            t_type: USER_TYPE.CUSTOMER
-          },
+          user_to: { t_username: null, t_password: null, t_name: "Maria", t_surname: "Politano", t_alias_generated: "mariapolitano1", t_profile_photo: "https://cdn.21buttons.com/posts/640x799/a4f98433206c47f3ac3b47039996f26f_1080x1349.jpg", t_type: USER_TYPE.CUSTOMER },
+          user_at: { t_username: null, t_password: null, t_name: "Antonio", t_surname: "Lodato", t_alias_generated: "lodo", t_profile_photo: "https://scontent.fnap5-1.fna.fbcdn.net/v/t39.30808-6/336655652_1366549564142836_6189179333211279215_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=MMspqbZpIoEAX8SJzKR&_nc_ht=scontent.fnap5-1.fna&oh=00_AfCxMQHUITv5n3ssutheEupX0QdJ4fcrGeR0ACM3DoT_9w&oe=65DB9102", t_type: USER_TYPE.CUSTOMER },
           message: "Sei un bel ragazzo, lo sai? Blbllb",
           dateTime: moment("26/02/2024 12:40PM", "D/M/YYYY HH:mm:ss")
         }
       ]
     }
   ];
-  protected chatListGroupedByDate: Array<ChatDate> = [];
-  protected groupedByDateActiveChatUser: ChatDate = null;
-  protected showEmoticonPanel = false;
-
+  filteredListByActiveChat: Chat;
+  chatListGroupedByDate: ChatDate[] = [];
+  groupedByDateActiveChatUser: ChatDate = null;
+  showEmoticonPanel = false;
 
   constructor(private router: Router) {
     this.groupMessagesByDate();
   }
 
-  getLatestMessageElement(chat: ChatDate): Message {
-    let keysArray = Object.keys(chat.messages);
-    const messages = chat.messages[keysArray[keysArray.length -1]];
-    return messages && messages.length > 0 ? messages[messages.length - 1] : null;
+  ngAfterViewChecked(): void {
+    this.darkMode = localStorage.getItem("darkModeChoice") === "1";
+  }
+
+  onInputChange(): void {
+  }
+
+  getLatestMessageElement(chat: ChatDate): Message | null {
+    const lastKey = Object.keys(chat.messages).pop();
+    const messages = lastKey ? chat.messages[lastKey] : [];
+    return messages.length > 0 ? messages[messages.length - 1] : null;
   }
 
   groupMessagesByDate() {
-    this.chatListGroupedByDate = [];
-    this.chatList.forEach(chat => {
+    this.chatListGroupedByDate = this.chatList.map(chat => {
       const groupedMessages: { [date: string]: Message[] } = {};
 
       chat.messages.forEach(message => {
         const dateKey = message.dateTime.format("D/M/YYYY HH:mm");
 
-        if (!groupedMessages[dateKey]) {
-          groupedMessages[dateKey] = [];
-        }
-
+        groupedMessages[dateKey] = groupedMessages[dateKey] || [];
         groupedMessages[dateKey].push(message);
       });
-      this.chatListGroupedByDate.push({
+
+      return {
         userChat: chat.userChat,
         messages: groupedMessages
-      })
+      };
     });
   }
 
   groupMessagesByActiveChatUserByDate() {
-    this.groupedByDateActiveChatUser = null;
     const groupedMessagesByActiveUser: { [date: string]: Message[] } = {};
-
-    this.filteredListByActiveChat.messages.forEach(message => {
-
+    for (const message of this.filteredListByActiveChat.messages) {
       const dateKey = message.dateTime.format("D/M/YYYY HH:mm");
-
-      if (!groupedMessagesByActiveUser[dateKey]) {
-        groupedMessagesByActiveUser[dateKey] = [];
-      }
-
+      groupedMessagesByActiveUser[dateKey] = groupedMessagesByActiveUser[dateKey] || [];
       groupedMessagesByActiveUser[dateKey].push(message);
-    });
-
+    }
     this.groupedByDateActiveChatUser = {
       userChat: this.filteredListByActiveChat.userChat,
       messages: groupedMessagesByActiveUser
-    }
+    };
   }
 
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
   }
 
-  ngAfterViewChecked(): void {
-    let darkModeChoice = localStorage.getItem("darkModeChoice");
-    if (darkModeChoice === "0") {
-      this.darkMode = false;
-    } else {
-      this.darkMode = true;
-    }
-  }
-
-  onInputChange(): void {
-    console.log('Input changed:', this.messageValue);
-  }
-
   getMessageStatus(date: moment.Moment): string {
-    const now = moment(); // Data e ora correnti
-    const diffInHours = now.diff(date, 'hours'); // Differenza in ore tra la data fornita e adesso
+    const now = moment();
+    const diffInHours = now.diff(date, 'hours');
 
     if (diffInHours < 24) {
       return date.format("HH:mm");
-    } else if (diffInHours < 48) {
+    }
+
+    if (diffInHours < 48) {
       return this.translate("Yesterday");
-    } else if (diffInHours <= 24 * 5) {
+    }
+
+    if (diffInHours <= 24 * 5) {
       return this.translate(date.fromNow());
-    } else {
-      return date.format("DD/MM/YYYY HH:MM");
     }
+
+    return date.format("DD/MM/YYYY HH:mm");
   }
 
-  translate(str: string) {
-    let tmpStr: string = str;
-    if (str === "yesterday") {
-      return "Ieri"
-    } else {
-      tmpStr = tmpStr.replace("days", "giorni").replace("ago", "fa");
+  translate(str: string): string {
+    if (str === "Yesterday") {
+      return "Ieri";
     }
-    return tmpStr;
+
+    return str.replace("days", "giorni").replace("ago", "fa");
   }
 
-  setActiveUser(el: User) {
-    this.activeChatUser = el;
-    this.filteredListByActiveChat = this.chatList.filter(chatEl => chatEl.userChat.t_alias_generated === el.t_alias_generated)[0];
+  setActiveUser(user: User) {
+    this.activeChatUser = user;
+    this.filteredListByActiveChat = this.chatList.find(chat => chat.userChat.t_alias_generated === user.t_alias_generated);
     this.groupMessagesByActiveChatUserByDate();
   }
 
@@ -241,20 +177,28 @@ export class MessageListComponent implements AfterViewChecked {
   addEmoticonToChat(emoji: string) {
     const inputElement = this.messageInput.nativeElement;
     const currentCursorPosition = inputElement.selectionStart;
-    this.messageValue = this.messageValue.slice(0, currentCursorPosition) + emoji + this.messageValue.slice(currentCursorPosition);
+    this.messageValue = `${this.messageValue.slice(0, currentCursorPosition)}${emoji}${this.messageValue.slice(currentCursorPosition)}`;
     inputElement.setSelectionRange(this.messageValue.length, this.messageValue.length);
     inputElement.focus();
   }
 
   sendMessage() {
-    if (this.messageValue.length > 0) {
-      let chat = this.chatList.filter(el => el.userChat.t_alias_generated === this.activeChatUser.t_alias_generated)[0];
-      chat.messages.push({
+    if (this.messageValue.length === 0) {
+      return;
+    }
+
+    const activeChatAlias = this.activeChatUser.t_alias_generated;
+    const chat = this.chatList.find(el => el.userChat.t_alias_generated === activeChatAlias);
+
+    if (chat) {
+      const newMessage = {
         user_to: this.currentUser,
         user_at: this.activeChatUser,
         message: this.messageValue,
         dateTime: moment(moment(), "DD/MM/YYYY HH:MM:SS")
-      });
+      };
+
+      chat.messages.push(newMessage);
       this.groupMessagesByDate();
       this.groupMessagesByActiveChatUserByDate();
       this.messageValue = '';
