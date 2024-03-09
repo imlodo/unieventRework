a7b31edd07492a3a5658fd895de3934722d02584
Raven <55733870+imlodo@users.noreply.github.com>
2024-03-09 23:56:11 +0100

    Implementazione menu sx
    Implementazione menu sx

--- src/app/core/components/navbar-left-menu/navbar-left-menu.component.ts
+++ src/app/core/components/navbar-left-menu/navbar-left-menu.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'unievent-navbar-left-menu',
   templateUrl: './navbar-left-menu.component.html',
   styleUrls: ['./navbar-left-menu.component.scss']
 })
-export class NavbarLeftMenuComponent {
+export class NavbarLeftMenuComponent implements AfterViewInit{
+  isDNone: boolean;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.updateIsDNone();
+  }
+
+  private updateIsDNone(): void {
+    this.isDNone = window.innerWidth < 992;
+    this.updateMargin();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updateIsDNone();
+  }
+
+  private updateMargin(): void {
+    if (!this.isDNone) {
+      this.renderer.setStyle(this.el.nativeElement, 'margin-right', '240px');
+    } else {
+      this.renderer.removeStyle(this.el.nativeElement, 'margin-right');
+    }
+  }
+ 
 }
