5b887b2de996c32cbea80dc43306ee695132f828
Raven <55733870+imlodo@users.noreply.github.com>
2024-04-08 16:40:02 +0200

    Terminato dettaglio ticket
    Terminato dettaglio ticket

--- src/app/modules/ticket/components/ticket-detail/ticket-detail.component.ts
+++ src/app/modules/ticket/components/ticket-detail/ticket-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import JsBarcode from 'jsbarcode';
-
+import moment from 'moment';
+import { OBJECT_MAP_TYPE, getTicketNameByType } from 'src/app/core/utility/global-constant';
 @Component({
   selector: 'unievent-ticket-detail',
   templateUrl: './ticket-detail.component.html',
@@ -8,12 +9,25 @@ import JsBarcode from 'jsbarcode';
 })
 export class TicketDetailComponent implements AfterViewInit {
   @ViewChild('barcodeImage') barcodeImage: ElementRef;
+  @ViewChild('ticketPrint') ticketPrint: ElementRef;
   ticket: any;
+  event: any;
 
   constructor() {
     this.ticket = {
       id: this.generateRandomId(),
+      type: {
+        TABLE: {
+          DISCOTECA: true
+        }
+      },
+      price: 109.99
     };
+    this.event = {
+      title: "Titolo Evento",
+      date: moment().format("DD/MM/YYYY HH:mm"),
+      image_url: "/assets/img/event-image-placeholder.jpg"
+    }
   }
 
   ngAfterViewInit(): void {
@@ -41,5 +55,48 @@ export class TicketDetailComponent implements AfterViewInit {
     } else {
       console.error('Elemento barcodeImage non trovato nel DOM.');
     }
+  }
+
+  getTicketNameByType() {
+    return getTicketNameByType(this.ticket.type);
+  }
+
+  printTicket(): void {
+    const printContents = this.ticketPrint.nativeElement.innerHTML;
+    const popupWindow = window.open('', '_blank');
+    popupWindow.document.open();
+    popupWindow.document.write(`
+      <html>
+        <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <title>Biglietto</title>
+          <style>
+          .ticket-img {
+            height: 150px;
+            width: -webkit-fill-available;
+            object-fit: cover
+        }
+        
+        .ticket-img-container {
+            width: 100%;
+        }
+        
+        .barcodeimg {
+            height: 100px;
+            width: -webkit-fill-available;
+            object-fit: cover;
+        }
+        .ticket-container{
+          border: 1px dashed black;
+          width: 800px !important;
+        }
+          </style>
+        </head>
+        <body onload="window.print();window.close()">
+          ${printContents}
+        </body>
+      </html>
+    `);
+    popupWindow.document.close();
   }
 }
