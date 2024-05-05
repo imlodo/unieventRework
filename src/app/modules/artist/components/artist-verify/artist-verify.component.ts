bcaa913e184410daa440357d9fda3f314ffeb3bb
Raven <55733870+imlodo@users.noreply.github.com>
2024-05-05 15:40:55 +0200

    Implementazione verifica artista
    Implementazione verifica artista

--- src/app/modules/artist/components/artist-verify/artist-verify.component.ts
+++ src/app/modules/artist/components/artist-verify/artist-verify.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ExtendedFile } from 'src/app/core/utility/global-constant';
 
 @Component({
   selector: 'unievent-artist-verify',
@@ -6,9 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./artist-verify.component.scss']
 })
 export class ArtistVerifyComponent {
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement>;
+  uploadedFiles: Array<{ key: string, file: File }> = new Array();
 
-  sendVerifyAccount(){
+  sendVerifyAccount() {
     return false;
   }
 
+  handleFileInput(fileInput: HTMLInputElement, key: string) {
+    const files = fileInput.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files.item(i) as ExtendedFile;
+        if (file) {
+          file.preview = this.createFilePreview(file);
+          const existingUploadIndex = this.uploadedFiles.findIndex(upload => upload.key === key);
+          if (existingUploadIndex !== -1) {
+            this.uploadedFiles[existingUploadIndex] = { key: key, file: file };
+          } else {
+            this.uploadedFiles.push({ key: key, file: file });
+          }
+        }
+      }
+    }
+  }
+  
+
+  createFilePreview(file: ExtendedFile): string {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      // Ottieni l'anteprima dell'immagine come URL base64
+      if (typeof reader.result === 'string') {
+        file.preview = reader.result;
+      }
+    };
+    return '';
+  }
+
+  openUploadPanel(fileInput: HTMLInputElement) {
+    fileInput.click();
+  }
+
+  searchUploadedFile(key: string): { key: string, file: File } | undefined {
+    return this.uploadedFiles.find(upload => upload.key === key);
+  }
 }
