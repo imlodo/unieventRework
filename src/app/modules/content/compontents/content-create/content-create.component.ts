5668d0d85b63dcc4a0b328915ca1a0b6ce5f4eaa
Raven <55733870+imlodo@users.noreply.github.com>
2024-05-08 01:12:37 +0200

    Update Content create
    Implemented drag and drop feature

--- src/app/modules/content/compontents/content-create/content-create.component.ts
+++ src/app/modules/content/compontents/content-create/content-create.component.ts
@@ -8,13 +8,43 @@ import { Component } from '@angular/core';
 export class ContentCreateComponent {
   step:number = 1;
   selectedContentType:"Event"|"Topic"="Event";
+  selectedFile: File | null = null;
+  isArtist: boolean = true;
+  isDraggedOver: boolean = false;
+
+  constructor(){
+    this.step = this.isArtist ? 1 : 2;
+  }
 
   incrementStep(){
     this.step+=1;
   }
 
-  decrementStep(){
-    this.step-=1;
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.selectedFile = file;
+    this.incrementStep();
+  }
+
+  onFileDropped(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDraggedOver = false;
+    const file: File = event.dataTransfer.files[0];
+    this.selectedFile = file;
+    this.incrementStep();
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDraggedOver = true;
+  }
+
+  onDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDraggedOver = false;
   }
 
 }
